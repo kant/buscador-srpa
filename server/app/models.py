d46900756f1f2cb0924eba2be60ad296ccc8ec1a
Ignacio Heredia <ignacio.nh@gmail.com>
2016-09-21 19:01:07 -0300

    Empezando otros modelos

--- server/app/models.py
+++ server/app/models.py
@@ -3,7 +3,7 @@ from flask_user import UserMixin
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
 
     password = db.Column(db.String(255), nullable=False, server_default='')
     reset_password_token = db.Column(db.String(100), nullable=False, server_default='')
@@ -12,9 +12,6 @@ class User(db.Model, UserMixin):
     confirmed_at = db.Column(db.DateTime())
 
     active = db.Column('is_active', db.Boolean(), nullable=False, server_default='0')
-    first_name = db.Column(db.String(100), nullable=False, server_default='')
-    last_name = db.Column(db.String(100), nullable=False, server_default='')
-
     roles = db.relationship('Role', secondary='user_roles', backref=db.backref('users', lazy='dynamic'))
 
 
@@ -27,3 +24,48 @@ class UserRoles(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE'))
     role_id = db.Column(db.Integer(), db.ForeignKey('role.id', ondelete='CASCADE'))
+
+#
+# class Question(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#
+#
+# class RelatedQuestions(db.Model):
+#     pass
+#
+#
+# class Keyword(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     name = db.Column(db.String(200), unique=True)
+#
+#
+# class QuestionKeywords(db.Model):
+#     pass
+#
+#
+# class Answer(db.Model):
+#     pass
+#
+#
+# class Report(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     date = db.Column(db.Date())
+#
+#
+# class Tag(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     name = db.Column(db.String(200), unique=True)
+#
+#
+# class Author(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     name = db.Column(db.String(200), unique=True)
+#
+#
+# class Answerer(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     name = db.Column(db.String(200), unique=True)
+#
+#
+# class QuestionAnswerers(db.Model):
+#     pass
--- server/app/routes.py
+++ server/app/routes.py
@@ -4,6 +4,5 @@ from flask_user import login_required
 
 def init_routes(app):
     @app.route('/')
-    @login_required
     def home_page():
         return render_template('home.html')
