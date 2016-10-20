4b4b0159f1a6a6b274e9e8c6b1081c1865d17d35
Ignacio Heredia <ignacio.nh@gmail.com>
2016-10-20 11:06:32 -0300

    Empiezo funcionalidad de tageo sugerido en el front

--- server/app/static/js/jgm.js
+++ server/app/static/js/jgm.js
@@ -26,21 +26,74 @@ window.jgm.results = {
         })
     },
     bindTagEvents: function () {
-        $('button').tooltip({html:true, trigger: 'manual', placement: 'right'});
-
-        $('.result .btn.without-topic').on('click', function(e) {
-        });
-        $('.result .btn.without-subtopic').on('click', function(e) {
-            var $button = $(e.currentTarget);
+        var loadTags = function (e, tagType) {
+            var $button = $(e.currentTarget).addClass('disabled');
             var questionId = $button.closest('.result').data('question-id');
-            $button.tooltip('show').addClass('disabled');
-            $.post('/pregunta/' + questionId + '/sugerir_subtema', {}, function (response) {
-                var html = ''
-                for (var i=0; i<response.length; i++) {
-                    html += '<button type="button" class="btn btn-subtopic">' + response[i] + '</button>'
+            $button.qtip({
+                content: {
+                    text: function (event, api) {
+                        return showTags(api, questionId, tagType)
+                    }
+                },
+                position: {
+                    my: 'center left',
+                    at: 'center right'
+                },
+                hide: false,
+                style: {
+                    classes: 'qtip-jgm'
                 }
-                $button.siblings('.tooltip').find('.tooltip-inner').html(html);
+            }).qtip('show')
+        }
+
+        var showTags = function (api, questionId, tagType) {
+            var url;
+            if (tagType == 'topic') {
+                url = '/pregunta/' + questionId + '/sugerir_tema';
+            } else {
+                url = '/pregunta/' + questionId + '/sugerir_subtema';
+            }
+            $.ajax({
+                url: url,
+                method: 'GET'
+            }).then(function (response) {
+                content = '';
+                for (var i = 0; i < response.length; i++) {
+                    var button = '<button class="btn btn-primary" data-question-id="qid" data-tag-type="tagtype">text</button>';
+                    button = button.replace('text', response[i]);
+                    button = button.replace('qid', questionId);
+                    button = button.replace('tagtype', tagType);
+                    content += button;
+                }
+                api.set('content.text', $(content));
             })
+            return 'Cargando...'
+        };
+
+        var submitTags = function (e) {
+            var $button = $(e.currentTarget);
+            var questionId = $button.data('question-id');
+            var tagText = $button.text();
+            var tagType = $button.data('tag-type');
+            var data = {};
+            data[tagType] = tagText;
+            $.ajax({
+                url: '/pregunta/id/actualizar'.replace('id', questionId),
+                method: 'POST',
+                data: data
+            }).then(function (response) {
+                $button.parents('.qtip-jgm').remove();
+                $('.result[data-question-id=' + questionId + ']').replaceWith($(response));
+            })
+        }
+
+        $('body').on('click', '.qtip-jgm button', submitTags);
+
+        $('.result .btn.without-topic').on('click', function (e) {
+            loadTags(e, 'topic');
+        });
+        $('.result .btn.without-subtopic').on('click', function (e) {
+            loadTags(e, 'subtopic');
         });
     },
     bindEvents: function () {
