5855416839f8933536430ef299b5f6161f428daa
Ignacio Heredia <ignacio.nh@gmail.com>
2016-10-31 09:38:04 -0300

    Agrego delimitadores de palabras para highlight - close #16 close #25

--- server/app/static/js/jgm.js
+++ server/app/static/js/jgm.js
@@ -2,7 +2,16 @@ window.jgm = window.jgm || {};
 window.jgm.results = {
     highlight: function (questionId, words) {
         var question = $('.result[data-question-id=' + questionId + ']');
-        var options = {accuracy: 'exactly'};
+        var options = {
+            accuracy: {
+                value: 'exactly',
+                limiters: [
+                    '¡', '!', '@', '#', '&', '*', '(', ')', '-', '–', '—', '+',
+                    '=', '[', ']', '{', '}', '|', ':', ';', '\'', '\"', '‘',
+                    '’', '“', '”', ',', '.', '<', '>', '/', '¿', '?'
+                ]
+            }
+        };
         question.find('p.question-body').mark(words, options);
         question.find('p.question-context').mark(words, options);
     }
