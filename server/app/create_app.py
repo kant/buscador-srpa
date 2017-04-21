584a34506a9b0e20c4d3016f0b3966b20d6e73b5
Ignacio Heredia <ignacio.nh@gmail.com>
2017-04-21 12:53:54 -0300

    Agrego posibilidad de crear usuarios desde consola

--- server/app/create_app.py
+++ server/app/create_app.py
@@ -4,7 +4,8 @@ from flask.ext.babel import Babel
 from . import app, db, models
 from routes import init_routes
 from helpers import Searcher
-
+import getpass
+import os
 
 def create_db():
     app.config.from_object('app.config.Config')
@@ -15,21 +16,33 @@ def create_app():
     create_db()
     Babel(app)
     Mail(app)
-    init_users()
+    db_adapter = SQLAlchemyAdapter(db, models.User)
+    UserManager(db_adapter, app)
     searcher = Searcher()
     init_routes(app, db.session, searcher)
     return app
 
 
-def init_users():
+def create_user():
+    os.environ['SMTP_PASS'] = os.environ.get('SMTP_PASS', '')
+    app.config.from_object('app.config.Config')
     db_adapter = SQLAlchemyAdapter(db, models.User)
     user_manager = UserManager(db_adapter, app)
-    if not models.User.query.filter(models.User.email == 'admin@modernizacion.gob.ar').first():
-        admin_user = models.User(
-            email='admin@modernizacion.gob.ar',
-            active=True,
-            password=user_manager.hash_password('Modernizacion')
-        )
-        db.session.add(admin_user)
-        db.session.commit()
+    email = raw_input("User email: ")
+    if not models.User.query.filter(models.User.email == email).first():
+        password = getpass.getpass('User password: ')
+        password2 = getpass.getpass('Confirm user password: ')
+        if password == password2:
+            admin_user = models.User(
+                email=email,
+                active=True,
+                password=user_manager.hash_password(password)
+            )
+            db.session.add(admin_user)
+            db.session.commit()
+            print('User created successfully')
+        else:
+            print("The passwords don't match")
+    else:
+        print('The user already exists')
     return
